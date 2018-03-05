b2d8eaf7e08ed2a3036afe1868a899756beaa9a7
Paul Tiller <pftiller@gmail.com>
2018-03-05 11:18:11 -0600

    Going to grab new merge

--- server/public/scripts/controllers/header.controller.js
+++ server/public/scripts/controllers/header.controller.js
@@ -1,6 +1,21 @@
-myApp.controller('HeaderController', ['UserService', function(UserService) {
+myApp.controller('HeaderController', ['UserService', '$mdDialog', function(UserService, $mdDialog) {
     console.log('HeaderController created');
     var self = this;
     self.userService = UserService;
     self.userObject = UserService.userObject;
+
+
+    self.loginModal = function() {
+      $mdDialog.show({
+          controller: LoginController,
+          controllerAs: 'vm',
+          templateUrl: '../views/shared/login.html',
+          parent: angular.element(document.body),
+          targetEvent: ev,
+          clickOutsideToClose: true,
+
+      })
+
+  }
+
   }]);
