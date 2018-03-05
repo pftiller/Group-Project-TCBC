2f31d87cabfb7c4a0071b5943bd989d844daeb6f
Lukas Nord <luke.nord@gmail.com>
2018-03-05 13:00:39 -0600

    login functions moved from controller to service

--- server/public/scripts/controllers/login.controller.js
+++ server/public/scripts/controllers/login.controller.js
@@ -12,22 +12,8 @@ myApp.controller('LoginController', ['$http', '$location', 'UserService', functi
         self.message = "Enter your username and password!";
       } else {
         console.log('sending to server...', self.user);
-        $http.post('/api/user/login', self.user).then(
-          function (response) {
-            if (response.status == 200) {
-              console.log('success: on login ', response.data);
-              // location works with SPA (ng-route)g
-              
-              $location.path('/user');
-            } else {
-              console.log('failure error: ', response);
-              self.message = "Incorrect credentials. Please try again.";
-            }
-          },
-          function (response) {
-            console.log('failure error: ', response);
-            self.message = "Incorrect credentials. Please try again.";
-          });
+        UserService.login(self.user);
+        
       }
     };
                   /* Not in use for now */
--- server/public/scripts/services/user.service.js
+++ server/public/scripts/services/user.service.js
@@ -9,7 +9,6 @@ myApp.service('UserService', ['$http', '$location', function($http, $location){
         if(response.data.member_id) {
             // user has a curret session on the server
             console.log('user service: ', response.data);
-            
             self.userObject.member_id = response.data.member_id;
             console.log('UserService -- getuser -- User Data: ', self.userObject.member_id);
         } else {
@@ -21,7 +20,27 @@ myApp.service('UserService', ['$http', '$location', function($http, $location){
       console.log('UserService -- getuser -- failure: ', response);
       $location.path("/login");
     });
-  },
+  }
+
+  self.login = function(user){
+    $http.post('/api/user/login', user).then(
+      function (response) {
+        if (response.status == 200) {
+          console.log('success: on login ', response.data);
+          // location works with SPA (ng-route)g
+          
+          $location.path('/user');
+        } else {
+          console.log('failure error: ', response);
+          self.message = "Incorrect credentials. Please try again.";
+        }
+      },
+      function (response) {
+        console.log('failure error: ', response);
+        self.message = "Incorrect credentials. Please try again.";
+      });
+  }
+
 
   self.logout = function() {
     console.log('UserService -- logout');
