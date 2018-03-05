f3a4e08286921b39f8aa55e998727434969dc807
Patrick Connelly <patrickconnelly95@gmail.com>
2018-03-05 11:19:00 -0600

    move modal and details to service

--- server/public/scripts/controllers/member.myRides.controller.js
+++ server/public/scripts/controllers/member.myRides.controller.js
@@ -3,58 +3,9 @@ myApp.controller('MemberMyRidesController', ['RideDetailService', '$mdDialog', f
     let self = this;
 
     self.rideDetailReveal = function(ride){
-        console.log('BUTTON DETAILS CLICKED', ride);
-        self.loadWelcomeModal(ride);
+        RideDetailService.loadWelcomeModal(ride);
     }
     self.rides = RideDetailService.rides;
-
-    self.loadWelcomeModal = function (ride, ev) {
-        $mdDialog.show({
-            controller: RideDetailController,
-            controllerAs: 'vm',
-            templateUrl: '../views/partials/ride-detail-modal.html',
-            parent: angular.element(document.body),
-            targetEvent: ev,
-            clickOutsideToClose: true,
-            resolve: {
-                item: function () {
-                    return ride;
-                }
-            }
-        })
-
-    }
-
-    function RideDetailController($mdDialog, item , RideDetailService) {
-        const self = this;
-        self.rides = RideDetailService.rides;
-        self.ride = item;
-        self.user = {
-            loggedIn: false
-        };
-        self.hide = function () {
-            $mdDialog.hide();
-        };
-
-        self.cancel = function () {
-            $mdDialog.cancel();
-        };
-
-        self.success = function (answer) {
-            // console.log('answer', answer);
-            swal(answer, '', {
-                className: "success-alert",
-            });
-            // $mdDialog.hide(answer);
-        };
-        self.error = function (answer) {
-            // console.log('answer', answer);
-            swal(answer, '', 'error', {
-                className: "error-alert",
-            });
-            // $mdDialog.hide(answer);
-        };
-    }
 
   }]);
   
--- server/public/scripts/services/rideDetails.service.js
+++ server/public/scripts/services/rideDetails.service.js
@@ -1,9 +1,10 @@
-myApp.service('RideDetailService', ['$http', '$location', function ($http, $location) {
+myApp.service('RideDetailService', ['$http', '$location', '$mdDialog', function ($http, $location , $mdDialog) {
     console.log('RideDetailService Loaded');
     let self = this;
     self.rides = {
         list: []
     }
+
     self.getRideDetails = function () {
         return $http.get('/rideDetails')
             .then((response) => {
@@ -14,6 +15,53 @@ myApp.service('RideDetailService', ['$http', '$location', function ($http, $loca
                 console.log(err);
             })
     }
-
     self.getRideDetails();
+
+    self.loadWelcomeModal = function (ride, ev) {
+        $mdDialog.show({
+            controller: RideDetailController,
+            controllerAs: 'vm',
+            templateUrl: '../views/partials/ride-detail-modal.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose: true,
+            resolve: {
+                item: function () {
+                    return ride;
+                }
+            }
+        })
+
+    }
+
+    function RideDetailController($mdDialog, item , RideDetailService) {
+        const self = this;
+        self.rides = RideDetailService.rides;
+        self.ride = item;
+        self.user = {
+            loggedIn: false
+        };
+        self.hide = function () {
+            $mdDialog.hide();
+        };
+
+        self.cancel = function () {
+            $mdDialog.cancel();
+        };
+
+        self.success = function (answer) {
+            // console.log('answer', answer);
+            swal(answer, '', {
+                className: "success-alert",
+            });
+            // $mdDialog.hide(answer);
+        };
+        self.error = function (answer) {
+            // console.log('answer', answer);
+            swal(answer, '', 'error', {
+                className: "error-alert",
+            });
+            // $mdDialog.hide(answer);
+        };
+    }
 }]);
