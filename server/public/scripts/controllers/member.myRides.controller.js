d8f87d8ae2bc48bc0b86d78473171c1731b9e949
Patrick Connelly <patrickconnelly95@gmail.com>
2018-03-05 10:05:08 -0600

    basic layout sculpted

--- server/public/scripts/controllers/member.myRides.controller.js
+++ server/public/scripts/controllers/member.myRides.controller.js
@@ -7,10 +7,16 @@ myApp.controller('MemberMyRidesController', ['UserService', function(UserService
     self.rides = {
     list: [
         ride1 = {name: 'Ride Name', date: 'Ride Date/Time', category: 'Ride Category', description: 'Description poijoisgoisgoisgoisjdgjojg',
-        leader: 'Ride Leader', distance: 'Distance', gps: 'Gps Link'}
+        leader: 'Ride Leader', distancePicked: 'Distance picked', actualDistance: 'Distance Ridden', gps: 'Gps Link'},
+        ride2 = {name: 'Ride2 Name', date: 'Ride2 Date/Time', category: 'Ride2 Category', description: 'Description2 poijoisgoisgoisgoisjdgjojg',
+        leader: 'Ride2 Leader', distancePicked: 'Distance2 picked', actualDistance: 'Distance2 Ridden', gps: 'Gps Link2'}
     ]
     };
 
+    self.rideDetailReveal = function(ride){
+        console.log('BUTTON DETAILS CLICKED', ride);
+        alert(ride.name);
+    }
     // self.rides = RideService.rides;
     
   }]);
