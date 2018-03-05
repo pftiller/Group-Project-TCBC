1dd27d1b4a19c15868ce92816d236a459eb78130
Patrick Connelly <patrickconnelly95@gmail.com>
2018-03-05 11:15:00 -0600

    server route set up to get details

--- server/public/scripts/services/rideDetails.service.js
+++ server/public/scripts/services/rideDetails.service.js
@@ -1,15 +1,19 @@
-myApp.service('RideDetailService', ['$http', '$location', function($http, $location){
+myApp.service('RideDetailService', ['$http', '$location', function ($http, $location) {
     console.log('RideDetailService Loaded');
     let self = this;
-
     self.rides = {
-        list: [
-            ride1 = {name: 'Ride Name', date: 'Ride Date/Time', category: 'Ride Category', description: 'Description poijoisgoisgoisgoisjdgjojg',
-            leader: 'Ride Leader', distancesAvailable: ['40', '30', '20'], gps: 'Gps Link'},
-            ride2 = {name: 'Ride2 Name', date: 'Ride2 Date/Time', category: 'Ride2 Category', description: 'Description2 poijoisgoisgoisgoisjdgjojg',
-            leader: 'Ride2 Leader', distancesAvailable: ['20', '50', '100'], gps: 'Gps Link2'}
-        ]
-        };
-        
-  }]);
-  
+        list: []
+    }
+    self.getRideDetails = function () {
+        return $http.get('/rideDetails')
+            .then((response) => {
+                console.log(response.data);
+                self.rides.list = response.data.details;
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }
+
+    self.getRideDetails();
+}]);
