80663dc94d271b2c32f93a7409048cb26f24a9b0
Lukas Nord <luke.nord@gmail.com>
2018-03-05 13:08:02 -0600

    fixed issues with client.js and header

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,33 +6,35 @@ myApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $l
   $routeProvider
   // USERS 
   .when('/', {
-      redirectTo: 'home'
-    })
-    .when('/ride-leader/my-rides', {
-      templateUrl: '/views/ride-leader/templates/rideLeader.myRides.html',
-      controller: 'RideLeaderController as vm',
-      //resolve when users fleshed out more
-      // resolve: {
-      //   getuser : function(UserService){
-      //     return UserService.getuser();
-      //   }
-      // }
-    })
-    .when('/home', {
-      templateUrl: '/views/shared/home.html',
-      controller: 'HomeController as vm',
-    })
-    .when('/register', {
-      templateUrl: '/views/user/templates/register.html',
-      controller: 'LoginController as vm'
-    })
-    .when('/user', {
-      templateUrl: '/views/user/templates/user.html',
-      controller: 'UserController as vm',
-      resolve: {
-        getuser : function(UserService){
+    redirectTo: 'home'
+  })
+  .when('/ride-leader/my-rides', {
+    templateUrl: '/views/ride-leader/templates/rideLeader.myRides.html',
+    controller: 'RideLeaderController as vm',
+    //resolve when users fleshed out more
+    // resolve: {
+    //   getuser : function(UserService){
+    //     return UserService.getuser();
+    //   }
+    // }
+  })
+  .when('/home', {
+    templateUrl: '/views/shared/home.html',
+    controller: 'HomeController as vm',
+  })
+  .when('/register', {
+    templateUrl: '/views/user/templates/register.html',
+    controller: 'LoginController as vm'
+  })
+  .when('/user', {
+    templateUrl: '/views/user/templates/user.html',
+    controller: 'UserController as vm',
+    resolve: {
+      getuser : function(UserService){
           return UserService.getuser();
         }
+      }
+      })
   .when('/my-rides', {
     templateUrl: '/views/user/templates/member.myRides.html',
     controller: 'MemberMyRidesController as vm',
@@ -75,6 +77,7 @@ myApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $l
   })
   .otherwise({
     template: '<h1>404</h1>'
-  });
+  })
+  
   
 }]);
