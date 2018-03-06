63c3e746d19ae3d2471130a472f077a8b506769c
Paul Tiller <pftiller@gmail.com>
2018-03-06 12:02:31 -0600

    Here

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,12 +1,13 @@
 var myApp = angular.module('myApp', ['ngRoute', 'ngMaterial', 'ngMessages']);
 
 /// Routes ///
-myApp.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+myApp.config(['$routeProvider', '$locationProvider', '$mdThemingProvider', function ($routeProvider, $locationProvider, $mdThemingProvider) {
   console.log('myApp -- config')
   $routeProvider
     .when('/my-rides', {
       templateUrl: '/views/user/templates/member.myRides.html',
       controller: 'MemberMyRidesController as vm',
+      activetab: 'my-rides',
       resolve: {
         getuser: function (UserService) {
           return UserService.getuser();
@@ -16,6 +17,7 @@ myApp.config(['$routeProvider', '$locationProvider', function ($routeProvider, $
     .when('/ride-leader/my-rides', {
       templateUrl: '/views/ride-leader/templates/rideLeader.myRides.html',
       controller: 'RideLeaderController as vm',
+      activetab: 'my-rides',
       resolve: {
         getuser: function (UserService) {
           return UserService.getuser();
@@ -25,6 +27,7 @@ myApp.config(['$routeProvider', '$locationProvider', function ($routeProvider, $
     .when('/profile/my-profile', {
       templateUrl: '/views/profile/templates/my-profile.html',
       controller: 'MyProfileController as vm',
+      activetab: 'my-profile',
       resolve: {
         getuser: function (UserService) {
           return UserService.getuser();
@@ -41,6 +44,7 @@ myApp.config(['$routeProvider', '$locationProvider', function ($routeProvider, $
     .when('/home', {
       templateUrl: '/views/shared/home.html',
       controller: 'HomeController as vm',
+      activetab: 'home',
     })
     .when('/user', {
       templateUrl: '/views/user/templates/user.html',
@@ -68,6 +72,4 @@ myApp.config(['$routeProvider', '$locationProvider', function ($routeProvider, $
     .otherwise({
       template: '<h1>404</h1>'
     })
-
-
 }]);
