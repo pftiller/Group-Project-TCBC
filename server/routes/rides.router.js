82e66298ecaa6c7b5f9a84d14be55106e85f8a36
Lukas Nord <luke.nord@gmail.com>
2018-03-06 14:44:24 -0600

    merge conflict resolution on ride.router file

--- server/routes/rides.router.js
+++ server/routes/rides.router.js
@@ -3,88 +3,7 @@ const router = express.Router();
 const isAuthenticated = require('../modules/isAuthenticated');
 const pool = require('../modules/pool');
 const ridePackager = require('../modules/ridePackager.module');
-list = {
-    details: [
-        ride1 = {
-            id: 1,
-            rides_name: 'Cycle through the lakes',
-            rides_date: '02-02-2018',
-            rides_category: 'A',
-            description: 'Description poijoisgoisgoisgoisjdgjojg',
-            ride_location: '123 E Berry ST, St. Paul, MN',
-            ride_leader: 'Paul Tiller',
-            distances: ['40', '30', '20'],
-            selected_distance: '30',
-            actualDistance: '40',
-            url: 'Gps Link',
-            completed: false
-        },
-        ride3 = {
-            id: 2,
-            rides_name: 'Fun ride around',
-            rides_date: '01-17-2018',
-            rides_category: 'B',
-            description: 'Description poijoisgoisgoisgoisjdgjojg',
-            ride_leader: 'Lukas Nord',
-            ride_location: '123 E North ST, Minneapolis, MN',
-            distances: ['15', '25', '66'],
-            selected_distance: '15',
-            actualDistance: '66',
-            url: 'Gps Link',
-            completed: false
-        },
-        ride4 = {
-            id: 3,
-            rides_name: 'Hard cycle up mountain',
-            rides_date: '04-11-2018',
-            rides_category: 'C',
-            description: 'Description poijoisgoisgoisgoisjdgjojg',
-            ride_leader: 'Patrick Connelly',
-            distances: ['40', '80', '120'],
-            selected_distance: '120',
-            ride_location: '123 W Chester ST, Duluth, MN',
-            actualDistance: '40',
-            url: 'Gps Link',
-            completed: false
-        },
-        ride2 = {
-            id: 4,
-            rides_name: 'Ride2 Name',
-            rides_date: '03-03-2018',
-            rides_category: 'MB-A',
-            description: 'Description2 poijoisgoisgoisgoisjdgjojg',
-            ride_leader: 'Lukas Nord',
-            distances: ['20', '50', '100'],
-            selected_distance: '20',
-            ride_location: '123 S Mouth ST, Rochester, MN',
-            actualDistance: '50',
-            url: 'Gps Link2',
-            completed: true
-        }
-    ]
-};
 
-
-categories = {
-    options: [
-        'A – Very Strenuous',
-        'A/B – Strenuous',
-        'B – Brisk',
-        'B/C – Moderate',
-        'C – Relaxed',
-        'MB-A – Members Only',
-        'MB-AB – Member Only',
-        'MB-B – Members Only',
-        'MB-C – Members Only',
-        'N-A – Night',
-        'N-A/B – Night',
-        'N-B – Night',
-        'N-B/C – Night',
-        'N-C – Night',
-        'O – Outreach',
-        'S – Special'
-    ]
-}
 
 
         /* GET all rides*/ 
@@ -113,12 +32,9 @@ router.get('/public/details',  (req, res) => {
 
 
 
-<<<<<<< HEAD
-            /* GET All Categories */
+        
 
-=======
-            /* Fetch All Categories */
->>>>>>> 93026c107c33f89c319cf8ace7aaa3ad4e41060e
+            /* GET All Categories */
 router.get('/public/categories', (req, res) => {
     //res.send(categories);
     const CategoryQuery = `SELECT * FROM categories`;
