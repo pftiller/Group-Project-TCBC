120415252d57a3e7aa0d8e4c61ab1d8515cdf34b
Patrick Connelly <patrickconnelly95@gmail.com>
2018-03-06 14:57:53 -0600

    guest adds to db and modals

--- server/public/scripts/services/checkIn.service.js
+++ server/public/scripts/services/checkIn.service.js
@@ -39,6 +39,7 @@ myApp.service('CheckInService', ['$http', '$location', '$mdDialog', function ($h
         console.log('Ride to get users for ', rideId);
 
         //Get all riders signed up for the ride at this ride ID
+            //need some joins and such to find members tied to this ride
 
         // return $http.get(`/rides/rideLeader/signedUpRiders/${rideId}`)
         //     .then((response) => {
@@ -49,7 +50,7 @@ myApp.service('CheckInService', ['$http', '$location', '$mdDialog', function ($h
         //     })
     }
 
-
+    //when Ride Complete button clicked runs this funciton on current ride
     self.markRideComplete = function (rideId) {
         console.log('mark ride complete ', rideId);
         //Put request on proper ride id, change completed to true and then after that give members their mileage
@@ -60,5 +61,72 @@ myApp.service('CheckInService', ['$http', '$location', '$mdDialog', function ($h
             .catch((err) => {
                 console.log('ERR updating ride to complete ', err);
             })
+    }
+
+    //Add rider 
+    self.addMemberToRide = function(){
+        console.log('ADD RIDER ');
+        
+    }
+    self.addGuestToRide = function(){
+        console.log('ADD GUEST');
+        self.guestRegisterModal();
+    }
+
+    self.addGuestRider = function(guest){
+        console.log('ADD GUEST TO THIS RIDE ');
+        return $http.post(`/rides/rideLeader/addGuest`, guest)
+            .then((response)=>{
+                console.log('add guest to ride response ', response);
+            })
+            .catch((err)=>{
+                console.log('err adding guest to ride', err);
+            })
+    }
+
+
+    self.guestRegisterModal = function (ride, ev) {
+        $mdDialog.show({
+            controller: GuestRegisterController,
+            controllerAs: 'vm',
+            templateUrl: '../views/ride-leader/partials/guest-register-modal.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose: true,
+        })
+    }
+
+    function GuestRegisterController($mdDialog, CheckInService, $routeParams) {
+        const self = this;
+        let rideId = $routeParams.rideId
+        self.addGuestRider = function(){
+            console.log('guest ', self.newGuest);
+            CheckInService.addGuestRider(self.newGuest)
+                // .then(()=>{
+                //     self.hide();
+                // })
+        }
+        self.hide = function () {
+                $mdDialog.hide();
+            };
+
+            self.cancel = function () {
+                $mdDialog.cancel();
+            };
+
+            self.success = function (answer) {
+                // console.log('answer', answer);
+                swal(answer, '', {
+                    className: "success-alert",
+                });
+                // $mdDialog.hide(answer);
+            };
+            self.error = function (answer) {
+                // console.log('answer', answer);
+                swal(answer, '', 'error', {
+                    className: "error-alert",
+                });
+                // $mdDialog.hide(answer);
+            };
     }
 }]);
