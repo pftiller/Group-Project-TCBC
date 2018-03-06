c1050387ff0a0cf5de4985f0f72a327dc1a742aa
Paul Tiller <pftiller@gmail.com>
2018-03-06 08:41:04 -0600

    Filtering functionality for homepage

--- server/public/scripts/controllers/home.controller.js
+++ server/public/scripts/controllers/home.controller.js
@@ -4,18 +4,11 @@ myApp.controller('HomeController', ['RideDetailService', '$timeout', '$q', '$log
   self.rides = RideDetailService.rides;
   self.categories = RideDetailService.categories;
   self.isOpen = false;
-  self.querySearch   = querySearch;
-    self.simulateQuery = false;
-    self.isDisabled = false;
-    // list of `state` value/display objects
-    // self.stations = loadAll($http);
-    self.querySearch = querySearch;
-    self.selectedItemChange = selectedItemChange;
-    self.searchTextChange = searchTextChange;
+  self.simulateQuery = false;
+  self.isDisabled = false;
 
-    //self.newState = newState;
-
-    function querySearch(query) {
+  // Ride Name Search
+self.querySearch = function(query) {
       var results = query ? self.categories.list.ride_names.filter(createFilterFor(query)) : self.categories.list.ride_names,
         deferred;
       if (!query) {
@@ -32,95 +25,26 @@ myApp.controller('HomeController', ['RideDetailService', '$timeout', '$q', '$log
       }
     }
 
-    function searchTextChange(text) {
-      $log.info('Text changed to ' + text);
+    self.searchTextChange = function(text) {
+      console.log('Search text changed');
     }
 
-    function selectedItemChange(item) {
-      $log.info('Item changed to ' + JSON.stringify(item));
+   self.selectedItemChange = function(item) {
+      console.log('Selected item changed');
     }
 
-    // function loadAll($http) {
-    //   var allStations = [];
-    //   var result = [];
-    //   $http.get('/Train/GetAllStations')
-    //     .then(function(response) {
-    //       allStations = response.data;
-    //       angular.forEach(allStations, function(station, key) {
-    //         result.push({
-    //           value: station.StationName.toLowerCase(),
-    //           display: station.StationName
-    //         });
-    //       })
-    //     });
-    //   return result;
-    // }
-
-    function createFilterFor(query) {
-      var lowercaseQuery = angular.lowercase(query);
-      return function filterFn(state) {
-        return (state.value.indexOf(lowercaseQuery) === 0);
-      };
-
-    }
-  
-
-
-
-
-
-
-
-
-
-
-
-
+// Ride Details
   self.rideDetailReveal = function (ride) {
     RideDetailService.loadWelcomeModal(ride);
   }
-  // Date Picker
-
-  self.today = function () {
-    self.date = new Date();
-  };
-  self.today();
-
-  self.clear = function () {
-    self.date = null;
-  };
-
-
-  // Live Search
-  // self.querySearch = function(query) {
-  //   var results = query ? self.states.filter(createFilterFor(query)) : self.states,
-  //     deferred;
-  //   if (self.simulateQuery) {
-  //     deferred = $q.defer();
-  //     $timeout(function () {
-  //       deferred.resolve(results);
-  //     }, Math.random() * 1000, false);
-  //     return deferred.promise;
-  //   } else {
-  //     return results;
-  //   }
-  // }
-
-  function querySearch (query) {
-      console.log('search');
-        var deferred = $q.defer();
-        $timeout(function() {
-            deferred.resolve(self.rides);
-        }, Math.random() * 500, false);
-        return deferred.promise;
-    };
-
-
 
   // Category Dropdown Filter
   self.categoryFilter = {};
+
+// Clear Filters
+
   self.clearFilters = function () {
-    self.categoryFilter = {};
+    categoryFilter = {};
     self.date = null;
   }
 
