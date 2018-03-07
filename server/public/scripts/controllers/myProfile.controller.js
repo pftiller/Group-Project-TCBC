98f60a9d15c46f261dd877367936aa27b02cd731
darrenrector91 <darrenrector91@gmail.com>
2018-03-07 11:01:12 -0600

    my profile WORKINGgit add .git add .git add .

--- server/public/scripts/controllers/myProfile.controller.js
+++ server/public/scripts/controllers/myProfile.controller.js
@@ -1,12 +1,12 @@
-myApp.controller('MyProfileController', ['MyProfileService', function(MyProfileService) {
+myApp.controller('MyProfileController', ['MyProfileService', function (MyProfileService) {
     console.log('MyProfileController created');
     let self = this;
-    self.userObject = UserService.userObject;
     self.viewProfile = {};
 
     self.viewProfile = function(){
-      UserService.viewProfile().then((res)=>{
+      MyProfileService.viewProfile().then((res)=>{
         console.log('back from database with the stuff', res);
+        self.viewProfile = res[0];
       })
     }
     self.viewProfile();
