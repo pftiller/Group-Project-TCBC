a931e8ad13ee36bad9764f9434b8f80630dfb61f
darrenrector91 <darrenrector91@gmail.com>
2018-03-07 14:02:02 -0600

    removed console logs from my profile controller/service

--- server/public/scripts/controllers/myProfile.controller.js
+++ server/public/scripts/controllers/myProfile.controller.js
@@ -1,11 +1,11 @@
 myApp.controller('MyProfileController', ['MyProfileService', function (MyProfileService) {
-    console.log('MyProfileController created');
+    // console.log('MyProfileController created');
     let self = this;
     self.viewProfile = {};
 
     self.viewProfile = function(){
       MyProfileService.viewProfile().then((res)=>{
-        console.log('back from database with the stuff', res);
+        // console.log('back from database with the stuff', res);
         self.viewProfile = res[0];
       })
     }
